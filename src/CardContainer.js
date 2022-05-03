c04071d891bbc520e7d625fb5d8cc6c7eddd83c9
Anita Lee <anita.lee@gmail.com>
2022-05-03 15:36:12 -0400

    WIP Shuffle feature

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -20,8 +20,11 @@ function CardContainer() {
   const [deck, setDeck] = useState({
     data: null,
     isLoading: true,
+
   })
-  const [cards, setCards] = useState([]);
+  const initialCardState = [];
+  const [cards, setCards] = useState(initialCardState);
+  const [shuffle, setShuffle] = useState(false);
 
   /** loads new deck on page mount */
   useEffect(function loadDeck() {
@@ -34,14 +37,35 @@ function CardContainer() {
     getDeck();
   }, [])
 
+  //FIXME: if want to useEffect, modify line 43 if conditional,
+  //otherwise use a button, see solution for button.
+  useEffect(function shuffleDeck() {
+    console.log("SHUFFLE DECK=", )
+    if (shuffle && deck.data) {reshuffle()}
+    /** Axios request to shuffle deck, update deck state */
+    async function reshuffle() {
+      const response = await axios.get(`${API_URL}/${deck.data.deck_id}/shuffle/`);
+      setDeck({data:response.data, isLoading:false})
+      setCards([]);
+    }
+  }, [shuffle, deck])
+
   /** Draws new card, update cards state */
   async function drawCard() {
-    if(cards.length === 52) return alert("Error: no cards remaining!");
+    // if(cards.length === 52) return alert("Error: no cards remaining!");
     const newCard = await axios.get(`${API_URL}/${deck.data.deck_id}/draw/?count=1`);
     setCards(cards => [...cards, newCard.data.cards[0]]);
   }
 
+  function setShuffleDeck(){
+    setShuffle(true);
+  }
+
   if (deck.isLoading) {return <i>Loading...</i>};
+  if (cards.length === 52) {
+    setShuffleDeck();
+    return <i>Loading...</i>;
+  };
 
   return (
     <div>
